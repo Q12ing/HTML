229084c6f134cb33e7ea11a9d664ce9de01c8070
Q12ing <simjang640@gmail.com>
2025-01-10 18:12:22 +0900

    250110 백업

--- javascript/script/function2.js
+++ javascript/script/function2.js
@@ -11,7 +11,7 @@ const danBtn = document.querySelector('#danBtn');
 const danResult = document.querySelector('.danResult');
 console.log(danInput, danBtn, danResult); //error, undefined, null check
 
-danBtn.addEventListener('click',function(){dan99(Number(danInput.value));}) //대입값
+danBtn.addEventListener('click',()=>dan99(Number(danInput.value))) //대입값
 function dan99(dan){ //변수생성
     let total = '';
     total = `${dan}X1=${dan*1}<br>`;
@@ -33,5 +33,80 @@ function dan99(dan){ //변수생성
     danResult.innerHTML += `${dan}X7=${dan*7}<br>`;
     danResult.innerHTML += `${dan}X8=${dan*8}<br>`;
     danResult.innerHTML += `${dan}X9=${dan*9}`; */
-
 }
+
+//===========================================
+//매개변수 없는 화살표 함수
+const greet = ()=>console.log('hello');
+const greet2 = () => {
+    let a = 10;
+    return console.log(a+10);
+}
+greet();
+greet2();
+
+//매개변수 있는 화살표 함수
+//일반함수인 경우
+function func1(a){
+    return a+100;
+}
+console.log(func1(10));
+//화살표함수인 경우
+const func2 =(a)=>a+100;
+console.log(func2(10));
+
+//DOM, Node 관계 속성 공부
+const nav_a1 = document.querySelector('nav a');
+const nav_a2 = document.querySelectorAll('nav a');
+const header = document.querySelector('header');
+
+console.log('==========next, previous 관계속성 공부')
+nav_a2[1];
+console.log(nav_a2[1].nextElementSibling);
+console.log(nav_a2[1].previousElementSibling);
+
+console.log(nav_a1, nav_a2);
+console.log(nav_a1.parentElement); //DOM요소 nav
+console.log(nav_a2[0].parentElement); //DOM요소 nav
+console.log(nav_a2[1].parentNode); //요소 노드 nav(공백, 텍스트 포함 검사)
+/* nav_a1.parentElement.style = 'background-color:aqua;'
+nav_a2[2].parentNode.style = 'padding:20px;' */
+//style을 같은 DOM요소에 연속해서 두번 이상 적용하고 싶은 경우 CSS속성명을 자바스크립트 키워드 방식의 Camel표기법으로 작성해야 한다.
+nav_a1.parentElement.style.backgroundColor = 'aqua';
+nav_a1.parentElement.style.padding = '20px';
+nav_a1.addEventListener('mouseover',()=>nav_a1.parentElement.style.borderBottom='2px solid #000');
+nav_a2[3].addEventListener('mouseover',()=>nav_a2[3].parentNode.style.backgroundColor='yellow');
+nav_a2[3].addEventListener('mouseout',()=>nav_a2[3].parentNode.style.background='none');
+//자식노드 childNodes, children
+console.log(nav_a1.childNodes);
+console.log(nav_a1.children); 
+console.log(header.childNodes);
+console.log(header.children); //h1, nav
+console.log(header.children[0].innerHTML); //안에 들어있는 태그 포함 모든 것 
+console.log(header.children[0].innerText); //안에 들어있는 내용 (텍스트)
+console.log(header.children[1].innerHTML);
+//header에 마우스 올렸을 때 자식h1의 "logo" -> "로고" 글자 변경하기
+header.addEventListener('mouseover',()=>header.children[0].children[0].innerText='로고');
+console.log('=================childElementCount 자식노드개수');
+console.log(header.childElementCount); //2 (장바구니갯수, 상품갯수 등에 사용)
+
+//================================== 마켓컬리 쇼핑몰 장바구니 갯수
+//1. "cart_num" 장바구니 초기값 0
+//2. "cart_item" 사용자가 장바구니에 물건 2개를 담았다.
+//3. "cart_num_all" 2번의 물건 개수에 따라 장바구니 페이지에 전체 상품 개수 출력
+
+//1. 장바구니 초기값
+let cart_num = 0;
+const cart_num_all = document.querySelector('.all_cart_num');
+const cart_item = document.querySelector('.cart_contents');
+cart_num_all.innerText = cart_num; //초기값 0 선언
+//2. 사용자가 물건 2개를 담았다.
+cart_num = cart_item.childElementCount;
+console.log(cart_num);
+cart_num_all.innerText = cart_num; //현재 진행기준 장바구니 개수 대입
+
+console.log('==========================')
+console.log(header.firstchild) //공백
+console.log(header.firstElementChild); //h1
+console.log(header.lastChild);//공백
+console.log(header.lastElementChild);//nav
