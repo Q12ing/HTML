5fd841a9dea1c25269168db8801cbb89002913f8
Q12ing <simjang640@gmail.com>
2025-01-13 18:03:50 +0900

    ohou 자바 연습

--- javascript/project_ohou/script/product.js
+++ javascript/project_ohou/script/product.js
@@ -12,20 +12,38 @@ console.log(thumnail[0].src, bigImage.src);
 console.log(thumnail[0].parentElement);
 
 //두번째 썸네일(thum2.jpg)에 마우스 올렸을 때 큰 이미지 경로 (big2.jpg) 변경
-
+function thum_remove(){
+    thumnail[0].parentElement.classList.remove('active');
+    thumnail[1].parentElement.classList.remove('active');
+    thumnail[2].parentElement.classList.remove('active');
+    thumnail[3].parentElement.classList.remove('active');
+    thumnail[4].parentElement.classList.remove('active');
+}
+function bigSrc(num){
+    return bigImage.src=`./images/big${num}.jpg`
+}
 thumnail[0].addEventListener('mouseover',()=>{
-    bigImage.src='./images/big1.jpg'
-})
+    bigSrc(1);
+    thum_remove();
+    thumnail[0].parentElement.classList = 'active';
+});
 thumnail[1].addEventListener('mouseover',()=>{
-    bigImage.src='./images/big2.jpg'
+    bigSrc(2);
+    thum_remove();
     thumnail[1].parentElement.classList = 'active';
-})
+});
 thumnail[2].addEventListener('mouseover',()=>{
-    bigImage.src='./images/big3.jpg'
-})
+    bigSrc(3);
+    thum_remove();
+    thumnail[2].parentElement.classList = 'active';
+});
 thumnail[3].addEventListener('mouseover',()=>{
-    bigImage.src='./images/big4.jpg'
-})
+    bigSrc(4);
+    thum_remove();
+    thumnail[3].parentElement.classList = 'active';
+});
 thumnail[4].addEventListener('mouseover',()=>{
-    bigImage.src='./images/big5.jpg'
-})
+    bigSrc(5);
+    thum_remove();
+    thumnail[4].parentElement.classList = 'active';
+});
