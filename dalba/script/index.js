09a79f9fa1581d3ec90aece527283ce66cb14acf
Q12ing <simjang640@gmail.com>
2025-02-06 11:52:17 +0900

    dalba

--- dalba/script/index.js
+++ dalba/script/index.js
@@ -1,23 +1,45 @@
+/* main bnr swiper */
 const mainDot = document.querySelectorAll('.main_bnr .main_dot li');
-const mainSlide = document.querySelectorAll('.main_swiper .swiper-slide');
-console.log(mainDot, mainSlide)
 const mainSwiper = new Swiper('.main_swiper',{
     autoplay:{
-        delay:1000,
+        delay:3000,
     },
     loop:true,
     effect:'fade',
-    on:{
-        slideChange:function(e){
-            for(let i of mainDot){
-                i.classList.remove('.dot_active');
-                this.realIndex+1==mainDot[e.realIndex].classList.add('.dot_active');
-            }
+    on: {
+        slideChange: function (e) {
+            // 모든 dot에서 dot_active 클래스 제거
+            mainDot.forEach(dot => dot.classList.remove('dot_active'));
+            // 현재 활성화된 슬라이드에 맞는 dot에 클래스 추가
+            mainDot[e.realIndex].classList.add('dot_active');
         },
     },
 })
+mainDot.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        mainSwiper.slideToLoop(index);
+    });
+});
 
-/* pagination:{
-    el:'.main_bnr .main_dot',   //요소 선택(CSS선택자 작성방식으로)
-    type:'fraction',   //모양 설정 bullets(기본값), fraction, progressbar
-    clickable:true,},   //페이지번호 클릭으로 해당 슬라이드 이동 가능}*/
+
+/* best, new product swiper */
+function productSettings() {
+    return {
+        slidesPerView:4,
+        spaceBetween:26
+    };
+}
+const bsAllSwiper = new Swiper('.bs_all_swiper',productSettings());
+const bsMakeUpSwiper = new Swiper('.bs_makeup_swiper',productSettings());
+const bsSkincareSwiper = new Swiper('.bs_skincare_swiper',productSettings());
+const npAllSwiper = new Swiper('.np_all_swiper',productSettings());
+const npMakeUpSwiper = new Swiper('.np_makeup_swiper',productSettings());
+const npSkincareSwiper = new Swiper('.np_skincare_swiper',productSettings());
+
+
+/* best, new product progress bar */
+const pbProgress = document.querySelectorAll ('.active_progress_bar');
+function progressBar() {
+    return {};
+}
+for(let i in pbProgress){console.log(i);}
